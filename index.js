3b0f80723b02d418c10f4cc6e9d657a8803ecada
Nawar <abdonawar80@gmail.com>
2022-06-24 19:40:16 +0200

    authentication done

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 const express = require('express');
-
+var mysql= require('mysql');
+const path = require('path');
+var cookieParser = require('cookie-parser');
+// initializing express
 const app = express();
 
+app.use(cookieParser());
+// db configuration for connection
+var dbconfig = require('./config/database');
+var db = mysql.createConnection(dbconfig.connection);
+
+// DB connection
+db.connect((err)=> {
+    if(err){
+        console.log(error)
+    }else {
+        console.log("MySQL connected...")
+    }
+})
+
+// static files 
+const publicDirectory = path.join(__dirname , './public');
+app.use(express.static(publicDirectory));
+// Parse Form Data
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.set('view engine', 'hbs');
+
+
+
+
+// app on port 8000
 app.listen('8000', () => {
     console.log('Server on port 8000')
 });
 
+
+// Routes
+
+app.use('/',require('./routes/pages'));
+app.use('/auth',require('./routes/auth'));
--- scripts/create_database.js
+++ scripts/create_database.js
@@ -1,16 +1,20 @@
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
-});
+var dbconfig = require('../config/database');
 
-connection.connect();
+var connection = mysql.createConnection(dbconfig.connection)
 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
+
+connection.query('CREATE DATABASE ' + dbconfig.database);
+connection.query('\
+CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
+    `firstName` VARCHAR(20) NOT NULL, \
+    `lastName` VARCHAR(20) NOT NULL, \
+    `email` VARCHAR(50) NOT NULL, \
+    `password` VARCHAR(1024) NOT NULL, \
+    `pic_path` VARCHAR(1024) NOT NULL DEFAULT "../images/default.png", \
+        PRIMARY KEY (`id`), \
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC) \
+)');
 
 connection.end();
